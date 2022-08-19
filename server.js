50b676ef8ceb49a5e113b1acf495c64999a867ef
Freelanceshala <111358658+Freelanceshala@users.noreply.github.com>
2022-08-19 08:36:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -219,7 +219,7 @@ app.delete(prefix_api + '/tweet/:id', function (req, res) {
 });
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
     host = host === '::' ? 'localhost' : host;
